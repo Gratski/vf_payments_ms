94b3191228ce5f8e346d1d879ab27c09e496883d
Divanio Silva <dcsilva@mail.ru>
2020-04-29 11:05:43 +0100

    Create a Spring boot project for an initial commit

---
+++ src/main/kotlin/com/vfit/vfpaymentsgateway/VfPaymentsGatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.vfit.vfpaymentsgateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VfPaymentsGatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<VfPaymentsGatewayApplication>(*args)
+}
---
+++ src/test/kotlin/com/vfit/vfpaymentsgateway/VfPaymentsGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vfit.vfpaymentsgateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VfPaymentsGatewayApplicationTests {
+
+	//@Test
+	fun contextLoads() {
+	}
+
+}
